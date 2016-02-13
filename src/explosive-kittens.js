c4bc4294a8fc11ae1ff7952ce7aa9c828774b449
Ricardo Paradela <rem.paradela@gmail.com>
2016-02-13 01:27:21 +0000

    Implemented some messages, and the onDrawCard.

--- src/explosive-kittens.js
+++ src/explosive-kittens.js
@@ -4,6 +4,7 @@
 const rx = require('rx');
 const _ = require('underscore-plus');
 var util = require('util');
+const Card = require('./card');
 
 const Deck = require('./deck');
 const SlackApiRx = require('./slack-api-rx');
@@ -63,8 +64,13 @@ class ExplosiveKittens {
   }
 
   chooseFirstPlayer() {
-    let i = Math.floor(Math.random() * this.players.length);
-    return this.players[i];
+    for(let p of this.players) {
+      console.log(util.inspect(p, false, null));
+      if(p.name == 'paradela') return p;
+    }
+    return null;
+    /*let i = Math.floor(Math.random() * this.players.length);
+    return this.players[i];*/
   }
 
   getPlayerById(id) {
@@ -87,11 +93,42 @@ class ExplosiveKittens {
     else return this.actionStack.length > 0;
   }
 
-  //This
+/*{ player:
+    { id: 'U02U88YH5',
+      name: 'paradela',
+      isInGame: true,
+      holeCards:
+      [ Card { type: 'MomaCat', image: 'momacat.png' },
+        Card { type: 'Buttuba', image: 'buttuba.png' },
+        Card { type: 'Nope', image: 'nope.png' },
+        Card { type: 'Attack', image: 'attack.png' },
+        Card { type: 'Defuse', image: 'defuse.png' } ]
+    },
+    type: 'draw',
+    description: 'Draw one card from the deck.'
+  }
+  */
   evaluatePlayerAction(stopAction) {
-    console.log('evaluatePlayerAction: ' + util.inspect(this.actionStack.pop(), false, null));
+    let action = this.actionStack[this.actionStack.length - 1];
+    console.log('evaluatePlayerAction: ' + util.inspect(action, false, null));
 
-    this.gameEnded.onNext(true);
+    this.notifyActionToPlayers(action);
+
+    switch(action.type){
+      case 'draw':
+        this.onDrawCard(action.player);
+        break;
+      case 'skip':
+        break;
+      case 'steel':
+        break;
+      case 'peek':
+        break;
+      default:
+        break;
+    }
+
+    //this.gameEnded.onNext(true);
   }
 
   // Private: Deals hole cards to each player in the game. To communicate this
@@ -114,18 +151,102 @@ class ExplosiveKittens {
       player.holeCards = this.playerHands[player.id];
 
       if (!player.isBot) {
-        let dm = this.playerDms[player.id];
-        if (!dm) {
-          SlackApiRx.getOrOpenDm.subscribe(({dm}) => {
-            this.playerDms[player.id] = dm;
-            dm.send(`Your initial hand is: ${this.playerHands[player.id]}`);
-          });
-        } else {
-          dm.send(`Your initial hand is: ${this.playerHands[player.id]}`);
-        }
+        this.sendMessageToPlayer(player, `Your cards: ${player.holeCards}`);
       } else {
         player.holeCards = this.playerHands[player.id];
       }
+    }
+  }
+
+  sendMessageToPlayer(player, message) {
+    if(player.isBot) return;
+    let dm = this.playerDms[player.id];
+    if (!dm) {
+      SlackApiRx.getOrOpenDm.subscribe(({dm}) => {
+        this.playerDms[player.id] = dm;
+        dm.send(message);
+      });
+    } else {
+      dm.send(message);
+    }
+  }
+
+  sendMessageToAll(message, exceptions=[]) {
+    for(let p of this.players) {
+      if(p.isBot) continue;
+      if(exceptions.indexOf(p) > -1) continue;
+      this.sendMessageToPlayer(p, message);
+    }
+  }
+
+  notifyActionToPlayers(action) {
+    this.sendMessageToAll(`${action.player.name} acted with: ${action.type}`, [action.player]);
+  }
+
+  notifyDrawedCard(currentPlayer, card) {
+    this.sendMessageToPlayer(currentPlayer, `:explodingkittens: You picked up a: ${card.type}`);
+    //If it is an explosion, notify all the others
+    if(card.type == Card.ExplodingKittenType()) {
+      this.sendMessageToAll(`${currentPlayer.name} picked up an Explosion... Poor guy :explodingkittens:`, [currentPlayer]);
+    }
+  }
+
+  onDrawCard(player) {
+    //Draw one card from the deck
+    var card = this.deck.drawCard();
+
+    this.notifyDrawedCard(player, card);
+
+    //Check if it is an explosion
+    if(card.type == Card.ExplodingKittenType()) { //handle explosion
+      var isDefused = false;
+
+      //Look for a Defuse card
+      for(var i = 0; i < player.holeCards.length; i++) {
+        var c = player.holeCards[i];
+        if(c.type == Card.DefuseType()) {
+          isDefused = true;
+          //Discard the defuse card
+          player.holeCards.splice(i, 1);
+          //TODO: Ask user where he wants to place the explosion
+          this.deck.putExplosion(card, 1);
+          break;
+        }
+      }
+      if(!isDefused) {
+        //TODO: player lost the game
+        player.isInGame = false;
+      }
+    }
+    else {
+      //Player finished his turn, add card to player's hand
+      player.holeCards.push(card);
+      //Notify the player about his new cards
+      this.sendMessageToPlayer(player, `Your cards: ${player.holeCards}`);
+    }
+    //every time a player draws a card, the card stack must be cleared
+    this.actionStack.length = 0;
+  }
+
+  onSkip(player) {
+    //control var used to validated that a user has a card for this action
+    var validAction = false;
+    for(var i = 0; i < player.holeCards.length; i++) {
+      var c = player.holeCards[i];
+      if(c.type == Card.SkipType()) {
+        validAction = true;
+        //Discard the defuse card
+        player.holeCards.splice(i, 1);
+        //This is a valid action so the cardStack must be cleared
+        this.cardStack.length = 0;
+        //And the skip card is now on top of cardStack
+        this.cardStack.push(c);
+        //TODO: Once it is a skip, it must now signal to pass the play to the next player
+      }
+    }
+    //player does not have a skip card, he must play another card instead
+    if(!validAction) {
+      //TODO: notify player to play another card.
     }
   }
 
